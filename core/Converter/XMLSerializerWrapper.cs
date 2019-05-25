76096e8d70def846e32a3bbc1efeedc655eabbe6
Akansha <akanshanhce@gmail.com>
2019-05-25 20:56:44 +0200

    adding base structure of project

---
+++ core/Converter/XMLSerializerWrapper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Company_Review.core.Converter
+{
+    class XMLSerializerWrapper
+    {
+        public static Object deserialize(Type t, String filePath)
+        {
+            StreamReader reader = new StreamReader(filePath);
+            XmlSerializer serializer = new XmlSerializer(t);
+            Object returnObject = serializer.Deserialize(reader);
+            reader.Close();
+            return returnObject;
+        }
+
+        public static void serialize(Type t, String filePath, Object pDataObject)
+        {
+            StreamWriter writer = new StreamWriter(filePath);
+            XmlSerializer serializer = new XmlSerializer(t);
+            serializer.Serialize(writer, pDataObject);
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
---
+++ core/Reviews.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_Review.core
+{
+    public class Reviews
+    {
+        public List<Review> customerReviews;
+    }
+    public class Review
+    {
+        public int id;
+        public int companyId;
+        public float ratings;
+        public string jobStatus;
+        public string jobLocation;
+        public int lastYearEmployer;
+        public string employmentStatus;
+        public List<Skill> skills;
+        public string reviewTitle;
+        public List<Pro> Pros;
+        public List<Con> Cons;
+        public string wouldYouRecommend;
+
+
+    }
+
+    public class Skill
+    {
+        public string name;
+    }
+    public class Pro
+    {
+        public string comment;
+    }
+    public class Con
+    {
+        public string comment;
+    }
+}
